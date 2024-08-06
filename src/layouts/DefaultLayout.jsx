cfb8108ede96609fa6bc795c26bbc62508a46e21
lukacafuta <luka.cafuta@gmail.com>
2024-08-06 12:09:51 +0200

    added DefaultLayout

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,10 @@
+import {Outlet} from "react-router-dom";
+
+export default function DefaultLayout() {
+    return (
+        <div>
+            <h1>Looking for an excuse?</h1>
+            {/*<Outlet />*/}
+        </div>
+    )
+}
