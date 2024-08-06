2ab98e4a5600d9de80b3b357747cbfeed4552cd8
lukacafuta <luka.cafuta@gmail.com>
2024-08-06 13:01:30 +0200

    added Outlet

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,10 +1,11 @@
 import {Outlet} from "react-router-dom";
 
 export default function DefaultLayout() {
+    console.log("DefaultLayout rendered");
     return (
         <div>
             <h1>Looking for an excuse?</h1>
-            {/*<Outlet />*/}
+            <Outlet/>
         </div>
     )
 }
